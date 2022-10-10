e8b8ebacc012593633c771438ae7a358a5258b2e
FEAvansh <vansh.frontendarmy@gmail.com>
2022-10-10 18:10:12 +0530

    "chnages"

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/Homepage";
 import ReviewDetails from "./pages/ReviewDetails";
 import Category from "./pages/Category";
 import SiteHeader from "./components/SiteHeader";
+import Banner from "./components/Banner";
 
 // apollo client
 const client = new ApolloClient({
-  uri: "https://admin-cms.herokuapp.com/graphql",
+  // uri: "https://admin-cms.herokuapp.com/graphql",
+  uri: "http://localhost:1337/graphql",
   cache: new InMemoryCache(),
 });
 
@@ -17,11 +19,13 @@ function App() {
     <Router>
       <ApolloProvider client={client}>
         <div className="App">
-          <SiteHeader />
+          {/*  <SiteHeader /> */}
+          <Banner/>
           <Routes>
-            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/" element={Banner}/>
+            {/* <Route exact path="/" element={<HomePage />} />
             <Route path="/details/:id" element={<ReviewDetails />} />
-            <Route path="/category/:id" element={<Category />} />
+            <Route path="/category/:id" element={<Category />} /> */}
           </Routes>
         </div>
       </ApolloProvider>
---
+++ src/components/Banner.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+// import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import "../components/all.css";
+
+// const REVIEWS = gql`
+//   query GetReviews {
+//     reviews {
+//       data {
+//         id
+//         attributes {
+//           rating
+//           title
+//           body
+//           categories {
+//             data {
+//               id
+//               attributes {
+//                 name
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+const PAGES = gql`
+  query GetPages {
+    pages {
+      data {
+        id
+        attributes {
+          title
+          description
+          ... on Page {
+            serviceList {
+              ... on ComponentPagesPages {
+                ServiceComponent {
+                  image {
+                    data {
+                      attributes {
+                        url
+                      }
+                    }
+                  }
+                  title
+                  itemList
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Banner = () => {
+  const { loading, error, data } = useQuery(PAGES);
+  //   // const { loading, error, data } = useFetch(
+  //   //   "http://localhost:1337/api/reviews"
+  //   // );
+
+  if (loading) {
+    return <p>Loading....</p>;
+  }
+  if (error) {
+    return <p>error....</p>;
+  }
+  //   console.log("data: ", data?.pages?.data[0]?.attributes);
+  const Data = data?.pages?.data[0]?.attributes;
+  console.log("Data: ", Data?.serviceList[0]?.ServiceComponent?.map((c)=> c.itemList?.map((l)=> l)));
+
+  return (
+    <div class="jumbotron">
+      <h1 class="display-4">{Data?.title}</h1>
+      <p class="lead">{Data?.description}</p>
+      <hr class="my-4" />
+      <div className="postionDiv">
+        {Data?.serviceList[0]?.ServiceComponent?.map((c) => (
+          <div className="cardView">
+            <div class="card cardStyle">
+              <img
+                src={`http://localhost:1337${c.image.data[0].attributes.url}`}
+                class="card-img-top imgClass"
+                alt="..."
+              />
+              <div class="card-body">
+              <h5 class="card-title h5Class">{c.title}</h5>
+              { c.itemList.map((l,index) => (
+                    <li  class="card-text sideLi">
+                    {l}
+                  </li>
+                  ))}
+                {/* <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                {/* <a href="#" class="btn btn-primary">
+                  LINK 
+                </a> */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p> */}
+    </div>
+  );
+};
+
+export default Banner;
